ce546141c4d46856dc8a5bc0c643402ad361205b
shing <apple@SKYPPK.local>
2016-11-20 01:27:13 +0800

    new branch

--- src/java/cf/bean/UserInfo.java
+++ src/java/cf/bean/UserInfo.java
@@ -5,13 +5,116 @@
  */
 package cf.bean;
 
+import java.io.Serializable;
+
 /**
  *
  * @author apple
  */
-public class UserInfo {
+public class UserInfo implements Serializable{
     int id;
+    String loginId;
+    String password;
     String userName;
     String sex;
+    String date;
+    int tel;
+    String email;
+    String address;
+    double bonusPoints;
+
+    public UserInfo(int id, String loginId, String password, String userName, String sex, String date, int tel, String email, String address, double bonusPoints) {
+        this.id = id;
+        this.loginId = loginId;
+        this.password = password;
+        this.userName = userName;
+        this.sex = sex;
+        this.date = date;
+        this.tel = tel;
+        this.email = email;
+        this.address = address;
+        this.bonusPoints = bonusPoints;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public void setTel(int tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getBonusPoints() {
+        return bonusPoints;
+    }
+
+    public void setBonusPoints(double bonusPoints) {
+        this.bonusPoints = bonusPoints;
+    }
+    
     
 }
