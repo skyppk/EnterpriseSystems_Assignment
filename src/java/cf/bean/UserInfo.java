af3852d28d743d69ac89464c0a820be83b922a9e
shing <apple@SKYPPK.local>
2016-11-20 01:05:38 +0800

    Bean

---
+++ src/java/cf/bean/UserInfo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cf.bean;
+
+/**
+ *
+ * @author apple
+ */
+public class UserInfo {
+    int id;
+    String userName;
+    String sex;
+    
+}
